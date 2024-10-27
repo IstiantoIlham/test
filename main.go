fe03094cca83fea3b6aba48b8feb6717f6c8b634
IstiantoIlham <istianto.pahlawan@gmail.com>
2024-10-27 22:31:19 +0700

    Add function to reverse string

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func ReverseString(input string) string {
+    runes := []rune(input)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
+
+func main() {
+    fmt.Println(ReverseString("Hello World"))
+}
